478c71ef3877636f8862afe7a90ae71adc4401a0
tomarmonty <montytomar78@gmail.com>
2019-10-02 10:40:50 +0530

    heap added

--- gfg.py
+++ gfg.py
@@ -1,14 +1,3 @@
 
-def hextodec(st):
-    dic={'A':10,'B':11,'C':12,'D':13,'E':14,'F':15,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'0':0}
-    i=len(st)-1
-    decn=0
-    count=0
-    while i>=0:
-        decn+=dic[st[i]]*(16**count)
-        count+=1
-        i=i-1
-    return decn
 if __name__=="__main__":
-    st=str(input())
-    print(hextodec(st))
+
--- heap.py
+++ heap.py
@@ -1,7 +1,24 @@
-from heapq import heapify,heappop,heappush
-
-class minHeap:
-    def __init__(self):
-        self.heap=[]
-    def parent(self,i):
-        return (i-1)//2
+def heapify(arr,n,idx):
+    l=2*idx+1
+    r=2*idx+2
+    largest=idx
+    if l<n and arr[l] >  arr[idx]:
+        largest=l
+    if r< n and arr[r] > arr[largest]:
+        largest=r
+    if largest != idx :
+        arr[idx], arr[largest] = arr[largest], arr[idx]
+        heapify(arr,n,largest)
+def getmin(arr):
+    return arr[0]
+def heapsort(arr):
+    l=len(arr)
+    for i in range(l//2,-1,-1):
+        heapify(arr,l,i)
+    for i in range(l-1,0,-1):
+        arr[i],arr[0]=arr[0],arr[i]
+        heapify(arr,i,0)
+if __name__ == "__main__":
+    arr=[3,9,6,1,0,5]
+    heapsort(arr)
+    print(arr)
