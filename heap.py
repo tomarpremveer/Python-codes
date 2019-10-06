8c9a833b96332761e7941ea74302d33bf57646a0
tomarmonty <montytomar78@gmail.com>
2019-10-06 14:15:20 +0530

    heap1 added

--- heap.py
+++ heap.py
@@ -1,3 +1,5 @@
+import heapq
+from collections import Counter
 def heapify(arr,n,idx):
     l=2*idx+1
     r=2*idx+2
@@ -18,7 +20,67 @@ def heapsort(arr):
     for i in range(l-1,0,-1):
         arr[i],arr[0]=arr[0],arr[i]
         heapify(arr,i,0)
+def minimumcost(arr):
+    total_cost=0
+    while len(arr) !=1:
+        f,s=0,0
+        heapq.heapify(arr)
+        f=heapq.heappop(arr)
+        s=heapq.heappop(arr)
+        total_cost+=f+s
+        heapq.heappush(arr,f+s)
+    return total_cost
+
+def cookies(k, A):
+    heapq.heapify(A)
+    ts=0
+    count=0
+    while len(A)!=1 and A[0]<k:
+        if A:
+            f = heapq.heappop(A)
+        if A:
+            l=heapq.heappop(A)
+        else:
+            l=0
+        s=1*f+2*l
+        ts+=s
+        count+=1
+
+    if ts<k:
+        return -1
+    else:
+        return count
+
+
+def maxnumber(m,k):
+    arr=[0,0,0,0,0]
+    for i in range(m):
+        a,b=input().split(' ')
+        a,b=int(a),int(b)
+        arr[a]+=100
+        arr[b+1]-=100
+        arr=prefix(arr)
+    return max(arr)
+def prefix(arr):
+    presum=[0 for i in range(len(arr))]
+    presum[0]=arr[0]
+    for i in range(1,len(arr)):
+        presum[i]=presum[i-1]+presum[i]
+    return presum
+
+def maxdist(arr,k):
+    d={}
+    count=len(arr)
+    for i in arr:
+        if i in d:
+            count-=1
+            k-=1
+        else:
+            d[i]=1
+    return count-k
+
+
+
+
 if __name__ == "__main__":
-    arr=[3,9,6,1,0,5]
-    heapsort(arr)
-    print(arr)
+    print(maxdist([1,2,2,2],1))
---
+++ heap1.py
@@ -0,0 +1,27 @@
+from collections import Counter
+import heapq
+
+
+class Heap:
+    def __init__(self,arr):
+        self.dic=Counter(arr)
+        self.heap=[v for (i,v) in self.dic.items()]
+        heapq._heapify_max(self.heap)
+
+    def maxdist(self,k):
+        while k > 0:
+            v=heapq._heappop_max(self.heap)
+            v-=1
+            if v != 0:
+                heapq.heappush(self.heap,v)
+                heapq._heapify_max(self.heap)
+            k-=1
+        return len(self.heap)
+
+
+
+
+if __name__ == "__main__":
+    heap=Heap([1,2,2,2])
+    k=1
+    print(heap.maxdist(k))
