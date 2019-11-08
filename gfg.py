92ebc3bae25aed029f6c3448322e8ff2e87a1327
tomarmonty <montytomar78@gmail.com>
2019-11-08 14:37:32 +0530

    hj

--- gfg.py
+++ gfg.py
@@ -1,33 +1,39 @@
 import time
-def p(a,n):
-    if n==1:
+
+
+def p(a, n):
+    if n == 1:
         return 2
-    t=n//2
-    if n%2==0:
-        return p(a,t)*p(a,t)
+    t = n // 2
+    if n % 2 == 0:
+        return p(a, t) * p(a, t)
     else:
-        return p(a,t)*p(a,t+1)
+        return p(a, t) * p(a, t + 1)
+
+
 def dp(n):
-    d=[0]*(n+1)
-    d[0]=1
-    for i in range(1,n+1):
-        d[i]=d[i-1]*2
+    d = [0] * (n + 1)
+    d[0] = 1
+    for i in range(1, n + 1):
+        d[i] = d[i - 1] * 2
     return d[n]
 
+
 def removeduplicates(arr):
-    j=0
-    for i in range(0,len(arr)-1):
-        if arr[i]!= arr[i+1]:
-            arr[j]=arr[i]
-            j+=1
-    arr[j]=arr[len(arr)-1]
+    j = 0
+    for i in range(0, len(arr) - 1):
+        if arr[i] != arr[i + 1]:
+            arr[j] = arr[i]
+            j += 1
+    arr[j] = arr[len(arr) - 1]
     print(arr)
+
 
 def plusone(digits):
     carry = 1
     for i in range(len(digits) - 1, -1, -1):
         digits[i] += carry
-        if digits[i] % 10 == 0 and digits[i]!=0:
+        if digits[i] % 10 == 0 and digits[i] != 0:
             digits[i] = 0
             carry = 1
         else:
@@ -36,7 +42,27 @@ def plusone(digits):
         digits.insert(0, carry)
     return digits
 
-if __name__=="__main__":
-    arr=[1,0,0,0,0]
-    print(plusone(arr))
 
+def cherryPickup(grid):
+    """
+    :type grid: List[List[int]]
+    :rtype: int
+    """
+    r = len(grid)
+    c = [0]
+    return getch(0, 0, r, grid, c)
+
+
+def getch(n, m, r, grid, c):
+    if n >= r or m >= r:
+        return 0
+    if grid[n][m] == -1:
+        return 0
+    if grid[n][m] == 1:
+        return 1
+    return getch(n + 1, m, r, grid, c) + getch(n, m + 1, r, grid, c)
+
+
+if __name__ == "__main__":
+    grid=[[0,1,-1],[1,0,-1],[1,1,1]]
+    print(cherryPickup(grid))
